1c10340576d95d690c0d3f15889781fece262302
Raul Novelo <rnovelo.cruz98@gmail.com>
2020-11-28 20:15:48 -0600

    filtro múltiple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import contacts from './clients'
 
-function App() {
+function App () {
+  const [search, setSearch] = useState('')
+  // const [rows, setRows] = useState([])
+
+  function onChange (e) {
+    const value = e.target.value
+    const fields = value.split(' ') // separar cada uno de las queries
+
+    let results = contacts.rows // por defecto inicias con todos los elementos
+
+    // por cada campo encontrado
+    fields.forEach(el => {
+      const field = el.split(':') // separar campo por clave y valor
+      const aux = results.filter(contact => {
+        try {
+          // busqueda por cada campo
+
+          switch (field[0]) {
+            case 'lastname':
+              // caso lastname
+              if (
+                contact.name.toUpperCase().indexOf(field[1].toUpperCase()) !==
+                -1
+              )
+                return contact
+              break
+
+            default:
+              if (
+                contact[field[0]]
+                  .toUpperCase()
+                  .indexOf(field[1].toUpperCase()) !== -1
+              )
+                return contact
+              break
+          }
+        } catch (error) {
+          // caso base de busqueda por defecto
+          if (contact.name.toUpperCase().indexOf(field[0].toUpperCase()) !== -1)
+            return contact
+        }
+      })
+      results = aux
+    })
+    setSearch(e.target.value)
+    console.log(results) // actualizar el state
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <label htmlFor='site-search'>Search the site:</label>
+        <input
+          type='search'
+          id='site-search'
+          name='q'
+          value={search}
+          onChange={onChange}
+          aria-label='Search through site content'
+        />
+
+        <button>Search</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/clients.js
@@ -0,0 +1,200 @@
+const contactList = {
+  rows: [
+    {
+      id: 2001,
+      name: 'Jubin Chawla',
+      status: 'active',
+      create_at: '2020-12-01',
+      clientType: 'Person',
+      personId: '2001',
+      contact: '+1-344432739',
+      company: 'First Exclusive Limited',
+      owners: [
+        {
+          clientEmployeeId: '158358da-3638-4105-8963-5abe16ca5355',
+          clientId: '63b7acb2-6678-451f-ace2-2968d804592b',
+          name: 'ACME',
+          title: ''
+        }
+      ],
+      responsible: {
+        user: {
+          dateFormat: 'DF_001',
+          firstName: 'Einar',
+          hasProfileImage: false,
+          languageCode: 'en',
+          lastName: 'Guerrero',
+          name: 'Einar Guerrero',
+          numberFormat: 'MF_02',
+          position: {
+            userPositionId: '8067897d-8b74-49de-8d69-be0611516f03'
+          },
+          securityUserId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          timeFormat: '24HR',
+          timeZoneId: 'Central Standard Time (Mexico)',
+          title: '',
+          userId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          userName: 'einar.guerrero@izvanit.mx'
+        },
+        userRole: {
+          isDeleted: false,
+          isSystemItem: true,
+          listItemId: 'b7484a01-84d0-46f1-8e12-187411fc0b68',
+          name: 'Owner',
+          predefinedType: 0,
+          sortIndex: 0
+        }
+      }
+    },
+    {
+      id: 2002,
+      name: 'Mark Johnson',
+      clientType: 'Person',
+      personId: '2002',
+      email: 'mark.johnson@abc.com',
+      contact: '+1-34354234',
+      company: 'Second Exclusive Limited',
+      responsible: {
+        user: {
+          dateFormat: 'DF_001',
+          firstName: 'Einar',
+          hasProfileImage: false,
+          languageCode: 'en',
+          lastName: 'Guerrero',
+          name: 'Einar Guerrero',
+          numberFormat: 'MF_02',
+          position: {
+            userPositionId: '8067897d-8b74-49de-8d69-be0611516f03'
+          },
+          securityUserId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          timeFormat: '24HR',
+          timeZoneId: 'Central Standard Time (Mexico)',
+          title: '',
+          userId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          userName: 'einar.guerrero@izvanit.mx'
+        },
+        userRole: {
+          isDeleted: false,
+          isSystemItem: true,
+          listItemId: 'b7484a01-84d0-46f1-8e12-187411fc0b68',
+          name: 'Owner',
+          predefinedType: 0,
+          sortIndex: 0
+        }
+      }
+    },
+    {
+      id: '63b7acb2-6678-451f-ace2-2968d804592b',
+      name: 'ACME',
+      clientType: 'Company',
+      companyId: '2003',
+      email: 'shane.lee@mnz.com',
+      contact: '+1-324563332',
+      company: 'Third Exclusive Limited',
+      responsible: {
+        user: {
+          dateFormat: 'DF_001',
+          firstName: 'Einar',
+          hasProfileImage: false,
+          languageCode: 'en',
+          lastName: 'Guerrero',
+          name: 'Einar Guerrero',
+          numberFormat: 'MF_02',
+          position: {
+            userPositionId: '8067897d-8b74-49de-8d69-be0611516f03'
+          },
+          securityUserId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          timeFormat: '24HR',
+          timeZoneId: 'Central Standard Time (Mexico)',
+          title: '',
+          userId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          userName: 'einar.guerrero@izvanit.mx'
+        },
+        userRole: {
+          isDeleted: false,
+          isSystemItem: true,
+          listItemId: 'b7484a01-84d0-46f1-8e12-187411fc0b68',
+          name: 'Owner',
+          predefinedType: 0,
+          sortIndex: 0
+        }
+      }
+    },
+    {
+      id: 2004,
+      name: 'Chris Crains',
+      clientType: 'Person',
+      personId: '1234',
+      email: 'chris.crains@cde.com',
+      contact: '+1-233454323',
+      company: 'Fourth Exclusive Limited',
+      responsible: {
+        user: {
+          dateFormat: 'DF_001',
+          firstName: 'Einar',
+          hasProfileImage: false,
+          languageCode: 'en',
+          lastName: 'Guerrero',
+          name: 'Einar Guerrero',
+          numberFormat: 'MF_02',
+          position: {
+            userPositionId: '8067897d-8b74-49de-8d69-be0611516f03'
+          },
+          securityUserId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          timeFormat: '24HR',
+          timeZoneId: 'Central Standard Time (Mexico)',
+          title: '',
+          userId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          userName: 'einar.guerrero@izvanit.mx'
+        },
+        userRole: {
+          isDeleted: false,
+          isSystemItem: true,
+          listItemId: 'b7484a01-84d0-46f1-8e12-187411fc0b68',
+          name: 'Owner',
+          predefinedType: 0,
+          sortIndex: 0
+        }
+      }
+    },
+    {
+      id: 2005,
+      name: 'Justice League Corp',
+      clientType: 'Company',
+      personId: '1234',
+      email: 'jonathan.trott@ewr.com',
+      contact: '+1-324565345',
+      company: 'Fifth Exclusive Limited',
+      responsible: {
+        user: {
+          dateFormat: 'DF_001',
+          firstName: 'Einar',
+          hasProfileImage: false,
+          languageCode: 'en',
+          lastName: 'Guerrero',
+          name: 'Einar Guerrero',
+          numberFormat: 'MF_02',
+          position: {
+            userPositionId: '8067897d-8b74-49de-8d69-be0611516f03'
+          },
+          securityUserId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          timeFormat: '24HR',
+          timeZoneId: 'Central Standard Time (Mexico)',
+          title: '',
+          userId: '1968e7da-9d03-49f2-9a8c-f7bdcf01c6d6',
+          userName: 'einar.guerrero@izvanit.mx'
+        },
+        userRole: {
+          isDeleted: false,
+          isSystemItem: true,
+          listItemId: 'b7484a01-84d0-46f1-8e12-187411fc0b68',
+          name: 'Owner',
+          predefinedType: 0,
+          sortIndex: 0
+        }
+      }
+    }
+  ],
+  count: 5
+}
+export default contactList
